36560258e8d44b73bfa113159d54ee91be06df96
akira ogawa <oakira@outlook.com>
2020-02-22 17:22:46 +0900

    study1 change

--- study1/index.php
+++ study1/index.php
@@ -1,3 +1,16 @@
 <?php
+	$author = "Alfred E Newman";
+	
+	$out = <<<_END
+	This is a Headline
+
+	This is the first line.
+	This is the second.
+	- Written ny $author.
+	_END;
+	echo $out;
+	echo "This is line".__LINE__."of file".__FILE__;
+	var_dump(__LINE__);
+
 	
 ?>
